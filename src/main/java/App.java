958df61a0103c75b7d0b170793b012dd798196e8
tlitz10 <99214988+tlitz10@users.noreply.github.com>
2022-03-09 18:21:09 -0500

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) {
         System.out.println("Salutation");
+        System.out.println("I was added from github!!! bwa hahahahaha");
     }
 }
